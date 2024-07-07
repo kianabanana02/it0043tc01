8d1d62fb3c589f4b28623e5796ad4f9d9c501a9e
kianabanana02 <167260792+kianabanana02@users.noreply.github.com>
2024-07-07 20:55:32 +0800

    Update villasan_finals.js

--- villasan_finals.js
+++ villasan_finals.js
@@ -8,3 +8,12 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
